947d6db5403cd1ea2adcd6d4014c10652985a897
tophe <tophe@DESKTOP-EC8N5HK>
2018-09-23 08:04:08 -0500

    Updated adafruit.io

--- temperature.py
+++ temperature.py
@@ -1,47 +1,64 @@
+"""
+  - Add this line to /etc/rc.local (before the exit 0):
+  -   /home/pi/ONBOOT.sh 2> /home/pi/ONBOOT.errors > /home/pi/ONBOOT.stdout &
+  - Add the following ONBOOT.sh script to /home/pi and make it executable:
+  
+#!/bin/bash
+cd /home/pi
+python3 temperature.py
+  
+"""
+
 import smbus
 import time
 import socket
 import json
 import datetime
 
-T_SLEEP     = 10
-SERVER_IP   = '192.168.1.12'
-SERVER_PORT = 7654
-I2C_ADDR    = 0x18
+import requests
+
+T_SLEEP  = 60*5
+I2C_ADDR = 0x18
+
+post_headers = {
+    'X-AIO-Key':' SECRET KEY GOES HERE ', 
+    'Content-Type':'application/json'
+}
+
+post_url = 'https://io.adafruit.com/api/v2/topher_cantrell/feeds/freezer-temperature/data'
 
 bus = smbus.SMBus(1)
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while True:
 
     # Read the temperature register
     temp = bus.read_word_data(I2C_ADDR,0x05)
     
-    # Note that the upper/lower must be reversed
+    # The upper/lower must be reversed    
     upper_byte = temp & 0xFF
-    lower_byte = (temp >> 8) & 0xFF
+    lower_byte = (temp >> 8) & 0xFF    
+    temp = (upper_byte << 8) | lower_byte
     
     # See page 25 in the datasheet:
     # https://cdn-shop.adafruit.com/datasheets/MCP9808.pdf
     
-    # Mask off the threshold bits
-    upper_byte = upper_byte & 0x1F
+    # Hold on to the sign bit (1 means negative)
+    tsign = (temp>>12)&1
     
-    # Handle signs
-    if (upper_byte & 0x10) == 0x10:
-        upper_byte = upper_byte & 0x0F
-        temp = 256-(upper_byte*16+lower_byte/16)
-    else:
-        temp = upper_byte*16 + lower_byte/16
-        
+    # 12-bit value / 16
+    temp = (temp & 0x0FFF) / 16.0
+    if tsign:
+        # add in the sign if it is negative
+        temp -= 256.0    
+              
     #print(temp)    
     
     msg = {
-        'datetime' : str(datetime.datetime.now()),
-        'temp' : temp
+        'created_at' : str(datetime.datetime.now(datetime.timezone.utc).isoformat()),
+        'value' : str(temp)
         }
-   
-    sock.sendto(str.encode(json.dumps(msg)), (SERVER_IP,SERVER_PORT))
     
+    r = requests.post(post_url,data=json.dumps(msg),headers=post_headers)
+    #print(r.text)
+       
     time.sleep(T_SLEEP)
--- temperature_server.py
+++ temperature_server.py
@@ -1,11 +1,10 @@
 import socket
 
-SERVER_IP   = '192.168.1.12'
 SERVER_PORT = 7654
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-sock.bind((SERVER_IP,SERVER_PORT))
+sock.bind(("",SERVER_PORT))
 
 while True:    
     data, addr = sock.recvfrom(1024)
