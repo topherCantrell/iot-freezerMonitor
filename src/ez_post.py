8f9003bfa8884317ab27671905bea774b78db11b
Chris Cantrell <topherCantrell@gmail.com>
2020-03-14 07:47:21 -0500

    switching to pull model

--- src/ez_post.py
+++ src/ez_post.py
@@ -1,6 +1,5 @@
 import socket
 import json
-import time
 
 def _send_string_cr(sock,s):
     sock.send((s+'\n').encode())
@@ -10,7 +9,7 @@ def post_to_home_gateway(name,data):
     data = json.dumps(data)
     
     sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    sock.connect(('192.168.1.2',80))
+    sock.connect(('10.0.0.18',80))
     
     _send_string_cr(sock,'POST /data/'+name+' HTTP/1.1')
     _send_string_cr(sock,'Content-Type: application/json')
